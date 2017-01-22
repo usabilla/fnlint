dca9dbb9dbe52de3c819d476670f052136159978
pietvanzoen <piet@usabilla.com>
2017-01-22 10:57:45 +0100

    test: test against oldest supported node version

--- test/_helpers/setup.js
+++ test/_helpers/setup.js
@@ -1,8 +1,11 @@
+/* eslint no-console: 0 */
 'use strict';
 
 const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 const matchers = require('./matchers');
 const promiseMatchers = require('@pietvanzoen/jasmine-promise-matchers');
+
+console.log(`Testing with Node ${process.version}`);
 
 // Custom reporter
 if (!global.wallaby) {
