d8cbcaeffda75e1639a113185d661b8b549c7332
privateomega <privateomega@protonmail.com>
2018-01-19 23:11:38 +0530

    feat(file-extension-linter): added supporting test cases

--- test/module.spec.js
+++ test/module.spec.js
@@ -10,6 +10,15 @@ const TEST_CONFIG = {
   reporter: false
 };
 
+const EXT_TEST_CONFIG = {
+  basePath: './test/_helpers/extension-test-files',
+  files: '*.js',
+  extensions: true,
+  format: 'extension',
+  extensionName: 'config',
+  reporter: false
+};
+
 function expectResults(results) {
   expect(results.ok).toBe(false);
   expect(results.passing.length).toBe(2);
@@ -20,10 +29,24 @@ function expectResults(results) {
   expect(results.failing).toContain('/testFileThree.js');
 }
 
+function expectExtensionResults(results) {
+  expect(results.ok).toBe(false);
+  expect(results.passing.length).toBe(2);
+  expect(results.passing).toContain('/a.config.js');
+  expect(results.passing).toContain('/b.config.js');
+  expect(results.passing).not.toContain('/index.js');
+  expect(results.failing.length).toBe(1);
+  expect(results.failing).toContain('/index.js');
+}
+
 describe('fnlint', function() {
   describe('promise', function() {
     it('lints files using given file basePath, files glob, and matcher', function(done) {
       expect(fnlint.promise(TEST_CONFIG)).toResolve(done, expectResults);
+    });
+
+    it('Extension - lints files using given file basePath, files glob, and matcher', function(done) {
+      expect(fnlint.promise(EXT_TEST_CONFIG)).toResolve(done, expectExtensionResults);
     });
 
     it('lints directories when configured', function(done) {
@@ -39,6 +62,23 @@ describe('fnlint', function() {
       });
     });
 
+    it('Extension - lints directories when configured', function(done) {
+      const config = _.defaults({
+        files: '**/*.js',
+        format: 'extension',
+        extensions: true,
+        extensionName: 'test'
+      }, EXT_TEST_CONFIG);
+      expect(fnlint.promise(config)).toResolve(done, function(results) {
+        expect(results.ok).toBe(false);
+        expect(results.failing).toContain('/a.config.js');
+        expect(results.failing).toContain('/b.config.js');
+        expect(results.failing).toContain('/index.js');
+        expect(results.failing).toContain('/sampleDir/c.config.js');
+        expect(results.failing).toContain('/sampleDir/index.js');
+      });
+    });
+
     it('does not include directories when glob pattern is **/*', function(done) {
       const config = _.defaults({
         files: '**/*',
@@ -50,7 +90,22 @@ describe('fnlint', function() {
         expect(results.failing).not.toContain('/dirOne');
       });
     });
+
+    it('Extension - does not include directories when glob pattern is **/*', function(done) {
+      const config = _.defaults({
+        files: '**/*',
+        format: 'extension',
+        extensions: true,
+        extensionName: 'test'
+      }, EXT_TEST_CONFIG);
+      expect(fnlint.promise(config)).toResolve(done, function(results) {
+        expect(results.ok).toBe(false);
+        expect(results.failing).not.toContain('/sampleDir');
+      });
+    });
   });
+
+  
 
   describe('async', function() {
     it('lints files using given file basePath, files glob, and matcher', function(done) {
